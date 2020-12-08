f61d86e3d91667a5c0bde2091e56f389d9133f24
RomuloMileris <romulosmileris@gmail.com>
2020-12-08 20:59:30 +0000

    UCD commits

---
+++ Week 2 - Intermediate Python/28- Equality.py
@@ -0,0 +1,11 @@
+# Comparison of booleans
+print(True == False)
+
+# Comparison of integers
+print(-5 * 15 != 75)
+
+# Comparison of strings
+print("pyscript" == "PyScript")
+
+# Compare a boolean with a numeric
+print(True == 1)
