3bf62b01755ece04de84bbf5e22385bdaaf44873
RomuloMileris <romulosmileris@gmail.com>
2021-01-12 14:58:48 +0000

    UCD commits

---
+++ Week 8 - Introduction to Data Visualization with Seaborn/13-Plotting subgroups in line plots.py
@@ -0,0 +1,40 @@
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create line plot of model year vs. horsepower
+sns.relplot(x="model_year", y="horsepower",
+            data=mpg, kind="line",
+            ci=None)
+
+# Show plot
+plt.show()
+
+
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Change to create subgroups for country of origin
+sns.relplot(x="model_year", y="horsepower",
+            data=mpg, kind="line",
+            ci=None, style="origin",
+            hue="origin")
+
+# Show plot
+plt.show()
+
+
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Add markers and make each line have the same style
+sns.relplot(x="model_year", y="horsepower",
+            data=mpg, kind="line",
+            ci=None, style="origin",
+            hue="origin", markers=True,
+            dashes=False)
+
+# Show plot
+plt.show()
