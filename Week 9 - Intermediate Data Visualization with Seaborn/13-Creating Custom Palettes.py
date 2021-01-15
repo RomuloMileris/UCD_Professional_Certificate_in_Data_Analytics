a4a77df69a2440132cfa3e89c4a1674e3e02d086
RomuloMileris <romulosmileris@gmail.com>
2021-01-15 17:06:07 +0000

    UCD commits

---
+++ Week 9 - Intermediate Data Visualization with Seaborn/13-Creating Custom Palettes.py
@@ -0,0 +1,12 @@
+# Create the Purples palette
+sns.palplot(sns.color_palette("Purples", 8))
+plt.show()
+
+# Create the husl palette
+sns.palplot(sns.color_palette("husl", 10))
+plt.show()
+
+# Create the coolwarm palette
+sns.palplot(sns.color_palette("coolwarm", 6))
+plt.show()
+
---
+++ Week 9 - Intermediate Data Visualization with Seaborn/9-Comparing styles.py
@@ -0,0 +1,17 @@
+# Plot with a dark style
+sns.set_style('dark')
+sns.distplot(df['fmr_2'])
+plt.show()
+
+# Clear the figure
+plt.clf()
+
+
+# Plot with a whitegrid style
+sns.set_style('whitegrid')
+sns.distplot(df['fmr_2'])
+plt.show()
+
+# Clear the figure
+plt.clf()
+
